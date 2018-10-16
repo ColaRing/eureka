92630bcda7ad15458befee07e85b93e2299555cd
ColaRing <179468762@qq.com>
2018-10-16 15:54:30 +0800

    Initial commit

---
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//新加eureka注解
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
